a7ce1074e2f84e2badc973be5c9d05be65db6323
vadimbakaev <bakaev.vad@gmail.com>
2018-04-08 14:59:22 +0200

    Refactoring AuthController

--- server/app/controllers/AuthController.scala
+++ server/app/controllers/AuthController.scala
@@ -18,49 +18,61 @@ class AuthController @Inject()(
     cache: SyncCacheApi
 ) extends AbstractController(cc) {
 
+  private[this] lazy val baseUrl: String = s"https://${authConfig.domain}"
+
   def login(): Action[AnyContent] = Action { implicit request: Request[AnyContent] =>
     val state = RandomUtil.alphanumeric()
-    cache.set("state", state)
+    cache.set(AuthController.StateKey, state)
 
-    val query =
-      s"""
-          |authorize?
-          |client_id=${authConfig.clientId}
-          |&redirect_uri=${authConfig.callbackURL}
-          |&response_type=code
-          |&scope=openid profile
-          |&audience=${authConfig.audience}
-          |&state=$state
-       """.stripMargin.replaceAll("\n", "")
+    val queryString: Map[String, Seq[String]] = Map(
+      AuthController.ClientIdQueryKey     -> Seq(authConfig.clientId),
+      AuthController.RedirectUriQueryKey  -> Seq(authConfig.callbackURL),
+      AuthController.AudienceQueryKey     -> Seq(authConfig.audience),
+      AuthController.ResponseTypeQueryKey -> Seq("code"),
+      AuthController.ScopeQueryKey        -> Seq("openid profile"),
+      AuthController.StateQueryKey        -> Seq(state)
+    )
 
-    Redirect(s"https://${authConfig.domain}/$query")
+    Redirect(baseUrl + "/authorize", queryString)
   }
 
   def logout: Action[AnyContent] = Action {
-    Redirect(
-      s"https://${authConfig.domain}/v2/logout?client_id=${authConfig.clientId}&returnTo=${authConfig.logoutPage}"
-    ).withNewSession
+
+    val queryString: Map[String, Seq[String]] = Map(
+      AuthController.ClientIdQueryKey -> Seq(authConfig.clientId),
+      AuthController.ReturnToQueryKey -> Seq(authConfig.logoutPage)
+    )
+
+    Redirect(baseUrl + "/v2/logout", queryString).withNewSession
   }
 
   def callback(code: String, state: String): Action[AnyContent] = Action.async {
     implicit request: Request[AnyContent] =>
-      if (cache.get("state").contains(state)) {
+      if (cache.get(AuthController.StateKey).contains(state)) {
         tokenService
           .recoverToken(code)
           .map {
             case (idToken, accessToken) =>
-              Redirect(routes.HomeController.index())
-                .withSession(
-                  "idToken"     -> idToken,
-                  "accessToken" -> accessToken
-                )
+              Redirect(routes.HomeController.index()).withSession("idToken" -> idToken, "accessToken" -> accessToken)
           }
           .recover {
-            case e: IllegalStateException => Unauthorized(e.getMessage)
+            case e => Unauthorized(e.getMessage)
           }
       } else {
         Future.successful(BadRequest("Invalid state parameter"))
       }
   }
 
+}
+
+object AuthController {
+  val StateKey: String = "state"
+
+  val ClientIdQueryKey: String     = "client_id"
+  val RedirectUriQueryKey: String  = "redirect_uri"
+  val AudienceQueryKey: String     = "audience"
+  val ResponseTypeQueryKey: String = "response_type"
+  val ScopeQueryKey: String        = "scope"
+  val StateQueryKey: String        = "state"
+  val ReturnToQueryKey: String     = "returnTo"
 }
