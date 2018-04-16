dcf7ba72e0435310b5c72071d963249804e4861f
vadimbakaev <bakaev.vad@gmail.com>
2018-04-16 21:41:12 +0200

    Cosmetic fix LocalDateCodec

--- server/app/repositories/codec/LocalDateCodec.scala
+++ server/app/repositories/codec/LocalDateCodec.scala
@@ -6,10 +6,13 @@ import org.bson.codecs.{Codec, DecoderContext, EncoderContext}
 import org.bson.{BsonReader, BsonWriter}
 
 class LocalDateCodec extends Codec[LocalDate] {
+
+  private[this] val encoderClass: Class[LocalDate] = classOf[LocalDate]
+
   override def encode(writer: BsonWriter, value: LocalDate, encoderContext: EncoderContext): Unit =
     writer.writeDateTime(value.atStartOfDay(ZoneId.systemDefault()).toInstant.toEpochMilli)
 
-  override def getEncoderClass: Class[LocalDate] = classOf[LocalDate]
+  override def getEncoderClass: Class[LocalDate] = encoderClass
 
   override def decode(reader: BsonReader, decoderContext: DecoderContext): LocalDate =
     Instant.ofEpochMilli(reader.readDateTime()).atZone(ZoneId.systemDefault()).toLocalDate
